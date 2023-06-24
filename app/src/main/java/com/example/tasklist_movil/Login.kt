1940075127dda0dde46b363b2f0227608928f994
20212208 <20212208@itla.edu.do>
2023-06-24 16:39:40 -0400

    Conexion a la base de datos firebase y creacion de la funcionalidad de resgistrar e Login del usuario

--- app/src/main/java/com/example/tasklist_movil/Login.kt
+++ app/src/main/java/com/example/tasklist_movil/Login.kt
@@ -1,22 +1,57 @@
 package com.example.tasklist_movil
 
 import android.content.Intent
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.example.tasklist_movil.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 
 
-enum class ProviderType {
-    BASIC
-}
+
 class Login : AppCompatActivity() {
+
+
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.GoRestore.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.loginBtn.setOnClickListener{
+
+                val email = binding.coreoRegis.text.toString()
+                val pass = binding.passRegis.text.toString()
+
+                if (email.isNotEmpty() && pass.isNotEmpty()){
+                        firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                            if (it.isSuccessful){
+                                val intent = Intent(this , MainActivity::class.java)
+                                startActivity(intent)
+                            }else {
+                                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                            }
+                        }
+                }else {
+                    Toast.makeText(this, "HAY CAMPOS VACIOS", Toast.LENGTH_SHORT).show()
+
+                }
+            }
 
         val tvGoRegister = findViewById<TextView>(R.id.tv_go_to_register)
         tvGoRegister.setOnClickListener {
@@ -26,56 +61,16 @@ class Login : AppCompatActivity() {
         val tvgoRestore = findViewById<TextView>(R.id.GoRestore)
         tvgoRestore.setOnClickListener { goTorestore() }
 
-       // login()
     }
+
+
 
     private fun goToRegister() {
         val i = Intent(this, RegisterActivity::class.java)
         startActivity(i)
     }
 
-    /*private fun login() {
-        val enter_btn = findViewById<Button>(R.id.enter_btn)
-        val Name_Edit_Text = findViewById<EditText>(R.id.Name_Edit_Text)
-        val Email_Edit_Text = findViewById<EditText>(R.id.Email_Edit_Text)
-        val Password_Edit_Text = findViewById<EditText>(R.id.Password_Edit_Text)
-        val Confir_Password_Edit_Text = findViewById<EditText>(R.id.Confir_Password_Edit_Text)
-        enter_btn.setOnClickListener {
-            if (Name_Edit_Text.text.isNotEmpty() && Email_Edit_Text.text.isNotEmpty() && Password_Edit_Text.text.isNotEmpty()
-                && Confir_Password_Edit_Text.text.isNotEmpty()
-            ) {
-                FirebaseAuth.getInstance()
-                    .signInWithEmailAndPassword(
-                        Email_Edit_Text.text.toString(),
-                        Password_Edit_Text.text.toString()
-                    ).addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
 
-                        } else {
-                            showAlert()
-                        }
-                    }
-            }
-        }
-    }
-
-    private fun showAlert() {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("ERROR")
-        builder.setMessage("SE HA PRODUCIDO UN ERROR AL REGISTRAR EL USUARIO")
-        builder.setPositiveButton("Aceptar", null)
-        val dialog: AlertDialog = builder.create()
-        dialog.show()
-    }
-
-    private fun showHome(email: String, provider: ProviderType){
-        val homeIntent : Intent = Intent( this, Login::class.java).apply{
-            putExtra("email", email)
-            putExtra( "provider", provider.name )
-        }
-        startActivity(homeIntent)
-    }*/
 
     private fun goTorestore(){
         val i = Intent(this, RestoreActivity::class.java)
--- app/src/main/java/com/example/tasklist_movil/RegisterActivity.kt
+++ app/src/main/java/com/example/tasklist_movil/RegisterActivity.kt
@@ -1,83 +1,71 @@
 package com.example.tasklist_movil
 
-import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
-import androidx.core.content.ContextCompat.startActivity
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tasklist_movil.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
+import java.util.jar.Attributes.Name
 
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        //setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
 
+        binding.tvGoToLogin.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
 
+        binding.Registrarsebtn.setOnClickListener{
+            val name = binding.NameEditText.text.toString()
+            val email = binding.EmailEditText.text.toString()
+            val pass = binding.PasswordEditText.text.toString()
+            val confirmPass = binding.ConfirPasswordEditText.text.toString()
+
+            if (name.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()){
+                if (pass == (confirmPass)){
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful){
+                            val intent = Intent(this , Login::class.java)
+                            startActivity(intent)
+                        }else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+
+                }else{
+                    Toast.makeText(this, "LAS CONTRASENA NO ES IGUAL A LA CONFIRMACION", Toast.LENGTH_SHORT).show()
+                }
+            }else {
+                Toast.makeText(this, "HAY CAMPOS VACIOS", Toast.LENGTH_SHORT).show()
+
+            }
+        }
         val tvGoLogin = findViewById<TextView>(R.id.tv_go_to_login)
         tvGoLogin.setOnClickListener{
             goToLogin()
 
-            // configuration
-            config()
+
         }
     }
 
-    private fun config() {
-        val Registrarse_btn = findViewById<Button>(R.id.Registrarsebtn)
-        val Name_Edit_Text = findViewById<EditText>(R.id.Name_Edit_Text)
-        val Email_Edit_Text = findViewById<EditText>(R.id.Email_Edit_Text)
-        val Password_Edit_Text = findViewById<EditText>(R.id.Password_Edit_Text)
-        val Confir_Password_Edit_Text = findViewById<EditText>(R.id.Confir_Password_Edit_Text)
-        title = "Validacion"
-        Registrarse_btn.setOnClickListener {
-            val dialogBuilder = AlertDialog.Builder(this)
-            dialogBuilder.setMessage("Adiós")
-                .setCancelable(false)
-                .setPositiveButton("Aceptar", DialogInterface.OnClickListener { dialog, id ->
-                    // Acción a realizar al pulsar el botón "Aceptar" en el cuadro de diálogo
-                })
-            val alert = dialogBuilder.create()
-            alert.setTitle("Mensaje")
-            alert.show()
 
-           /* if (Name_Edit_Text.text.isNotEmpty() && Email_Edit_Text.text.isNotEmpty() && Password_Edit_Text.text.isNotEmpty()
-                && Confir_Password_Edit_Text.text.isNotEmpty()) {
-                FirebaseAuth.getInstance()
-                    .createUserWithEmailAndPassword(Email_Edit_Text.text.toString(),
-                        Password_Edit_Text.text.toString()).addOnCompleteListener {
-                            if (it.isSuccessful) {
-                                showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
 
-                            } else {
-                                showAlert()
-                            }
-                    }
-            }*/
-        }
-    }
-
-   /*private fun showAlert() {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("ERROR")
-        builder.setMessage("SE HA PRODUCIDO UN ERROR AL REGISTRAR EL USUARIO")
-        builder.setPositiveButton("Aceptar", null)
-        val dialog: AlertDialog = builder.create()
-        dialog.show()
-    }*/
-
-    /*private fun showHome(email: String, provider: ProviderType){
-        val homeIntent : Intent = Intent( this, Login::class.java).apply{
-            putExtra("email", email)
-            putExtra( "provider", provider.name )
-        }
-        startActivity(homeIntent)
-    }*/
     private fun goToLogin() {
         val i = Intent(this, Login::class.java)
         startActivity(i)
